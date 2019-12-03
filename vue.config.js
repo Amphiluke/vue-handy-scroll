3a0d14074b44525556934bac783ba1e23ff1206c
Amphiluke <amphiluke@gmail.com>
2019-12-03 19:52:02 +0700

    Correct publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-handy-scroll/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-handy-scroll/dist/" : "/"
 };
